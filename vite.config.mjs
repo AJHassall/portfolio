5bc07f4ee83e9a44108dadb1ab0823e23131baca
Adam Hassall <ajhassall2000@gmail.com>
2025-03-06 16:49:11 +0000

    gh pages fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const bae = process.env.NODE_ENV === 'ghpages'
+  ? '/portfolio/'
+  : '';
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
+    base: "base"
   },
 });
