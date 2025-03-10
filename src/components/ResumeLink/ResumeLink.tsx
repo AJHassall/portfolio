5bcb5830385cb2685064a164ee51a910efafa2f7
adam <ajhassall2000@gmail.com>
2025-03-10 01:11:28 +0000

    fix: resume download

--- src/components/ResumeLink/ResumeLink.tsx
+++ src/components/ResumeLink/ResumeLink.tsx
@@ -1,9 +1,10 @@
 import { Anchor, Group, Text } from '@mantine/core';
 import { IconArrowUpRight } from '@tabler/icons-react'; // Import the arrow icon
+import resume from '/Adam_H_CV.pdf'
 
 export function DownloadResumeLink() {
   return (
-    <Anchor href="portfolio/Adam_H_CV.pdf" download> {/* Replace with the actual path to your resume */}
+    <Anchor href={resume}> {/* Replace with the actual path to your resume */}
       <Group gap="xs" justify="flex-end">
         <Text >See my full résumé</Text>
         <IconArrowUpRight size={16} />
